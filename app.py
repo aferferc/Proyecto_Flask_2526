bbbc66cf13650cbecf127def0ad664709c0638a2
Alfredo Fernandez <aferferc@g.educaand.es>
2026-05-06 18:59:18 +0200

    E añadido la pagina items con un cuestionario que puede recordar estado

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, abort, redirect
+from funciones import *
 
 app = Flask(__name__)
+
+datos=cargarDatos('toyota-modificado.json')
 
 @app.route('/')
 def inicio():
@@ -8,6 +11,20 @@ def inicio():
 
 @app.route('/items')
 def items():
-    return render_template('items.html')
+    busqueda = request.args.get('busqueda', '')
+    modelo_sel = request.args.get('modelo', '')
+    orden = request.args.get('orden', 'asc')
+    modelos_disp = obtenerModelos(datos)
+    
+    
+    print(busqueda, modelo_sel, orden)
+    
+    
+    return render_template('items.html',
+                           modelos = modelos_disp,
+                           busqueda = busqueda,
+                           modelo_sel = modelo_sel,
+                           modelos_disp = modelos_disp,
+                           orden = orden)
 
 app.run('0.0.0.0', 5000, debug=True)
